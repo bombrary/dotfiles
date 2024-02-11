80a4e8699cd730f7eacf9dca45d91f606a5bc098
bombrary <bombra108@gmail.com>
2024-02-11 19:44:08 +0900

    add nix configs

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "NixOS Configuration";
+
+  inputs = {
+    nixos.url = github:NixOS/nixpkgs/nixos-23.11;
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    dotfiles = {
+      url = "./dotfiles";
+      flake = false;
+    };
+  }; 
+
+  outputs = { self, nixos, home-manager, nixpkgs, dotfiles, ... }: {
+    nixosConfigurations = {
+      minimal = nixos.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./os-configs/minimal/configuration.nix ];
+      };
+    };
+
+    homeConfigurations = {
+      bombrary = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+        };
+        extraSpecialArgs = {
+          inherit dotfiles;
+        };
+        modules = [
+          ./home-configs/bombrary/home.nix
+        ];
+      };
+    };
+  };
+}
---
+++ os-configs/minimal/configuration.nix
@@ -0,0 +1,42 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  time.timeZone = "Asia/Tokyo";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  users.users.bombrary = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+
+  services.openssh.enable = true;
+
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "23.11"; # Did you read the comment?
+}
+
