a951f0d68dc9100ce9fc212c44ef594a3f8cb934
bombrary <bombra108@gmail.com>
2024-04-21 13:30:45 +0000

    update nvim configs

--- config/nvim/lua/plugins/ddu.lua
+++ config/nvim/lua/plugins/ddu.lua
@@ -1,299 +1,111 @@
 local function get_current_buf_text()
   local _, srow, scol, _ = unpack(vim.fn.getpos("."))
   local _, erow, ecol, _ = unpack(vim.fn.getpos("v"))
-
   if srow < erow or (srow == erow and scol <= ecol) then
     return vim.api.nvim_buf_get_text(0, srow-1, scol-1, erow-1, ecol, {})
   else
     return vim.api.nvim_buf_get_text(0, erow-1, ecol-1, srow-1, scol, {})
   end
-
 end
 
 return {
   { 
-    "ddu-test",
-    dir = "~/tmp/ddu-test"
-  },
-  {
     "Shougo/ddu.vim",
     dependencies = {
       "vim-denops/denops.vim",
-      "Shougo/ddu-source-file_rec",
-      "Shougo/ddu-source-buffer",
-      "Shougo/ddu-source-rg",
-      "yuki-yano/ddu-filter-fzf",
-      "uga-rosa/ddu-filter-converter_devicon",
-      "Shougo/ddu-kind-file",
       "Shougo/ddu-ui-ff",
-      "ddu-test",
+      "Shougo/ddu-source-file_rec",
+      "shun/ddu-source-buffer",
+      "shun/ddu-source-rg",
+      "yuki-yano/ddu-filter-fzf",
+      "Shougo/ddu-kind-file",
     },
     config = function()
-      local lines = vim.opt.lines:get()
-      local height, row = math.floor(lines * 0.8), math.floor(lines * 0.1)
-      local columns = vim.opt.columns:get()
-      local width, col = math.floor(columns * 0.8), math.floor(columns * 0.1)
-
-      vim.fn['ddu#custom#patch_global']({
-        ui = 'ff',
-        sources = {{name = 'file_rec', params = {}}},
+      vim.fn["ddu#custom#patch_global"]({
+        ui = {
+          name = "ff",
+        },
         sourceOptions = {
           _ = {
-            matchers = {'matcher_fzf'},
-            sorters = {'sorter_fzf'},
-            converters = { 'converter_devicon' },
+            matchers = { "matcher_fzf" },
+            sorters = { "sorter_fzf" },
           },
-        },
-        sourceParams = {
-          rg = {
-            args = { '--column', '--no-heading', '--color', 'never', '--json'},
-          }
         },
         filterParams = {
           matcher_fzf = {
-            highlightMatched = 'Search',
+            highlightMatched = "Search",
           },
         },
         kindOptions = {
           file = {
-            defaultAction = 'open',
-          },
-          sample = {
-            defaultAction = 'echo',
-          },
-        },
-        uiParams = {
-          ff = {
-            startFilter = true,
-            prompt = "> ",
-            cursorPos = 0,
-            split = "floating",
-            floatingBorder = "single",
-            filterFloatingPosition = "top",
-            --autoAction = {
-            --  name = "preview",
-            --},
-            --startAutoAction = true,
-            previewFloating = true,
-            previewFloatingBorder = "single",
-            previewSplit = "vertical",
-            previewFloatingTitle = "Preview",
-            previewWindowOptions = {
-              { "&signcolumn", "no" },
-              { "&foldcolumn", 0 },
-              { "&foldenable", 0 },
-              { "&number", 0 },
-              { "&wrap", 0 },
-              { "&scrolloff", 0 },
-            },
-            highlights = {
-              floating = "Normal",
-              floatingBorder = "Normal",
-            },
-            ignoreEmpty = true,
+            defaultAction = "open",
           },
         },
       })
 
-      local function resize()
-        local lines = vim.opt.lines:get()
-        local height, row = math.floor(lines * 0.8), math.floor(lines * 0.1)
-        local columns = vim.opt.columns:get()
-        local width, col = math.floor(columns * 0.8), math.floor(columns * 0.1)
-        local previewWidth = math.floor(width / 2)
-
-        vim.fn['ddu#custom#patch_global']({
-          uiParams = {
-            ff = {
-              winHeight = height,
-              winRow = row,
-              winWidth = width,
-              winCol = col,
-              previewHeight = height,
-              previewRow = row,
-              previewWidth = previewWidth,
-              previewCol = col + (width - previewWidth),
+      vim.fn["ddu#custom#patch_local"]("files", {
+        sources = {
+          {
+            name = "file_rec",
+            params = {
+              path = vim.fn.expand("~"),
             },
           },
-        })
-      end
-
-      resize()
-
-      vim.api.nvim_create_autocmd("VimResized", {
-        callback = resize,
+        },
       })
 
-
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = 'ddu-ff',
-        callback = function(ev)
-          vim.keymap.set('n', '<CR>', function()
-            vim.fn['ddu#ui#do_action']('itemAction')
-          end, { silent=true, buffer=true })
-
-          vim.keymap.set('n', '<Space>', function()
-            vim.fn['ddu#ui#do_action']('toggleSelectItem')
-          end, { silent=true, buffer=true })
-
-          vim.keymap.set('n', 'i', function()
-            vim.fn['ddu#ui#do_action']('openFilterWindow')
-          end, { silent=true, buffer=true })
-
-          vim.keymap.set('n', 'p', function()
-            vim.fn['ddu#ui#do_action']('preview')
-          end, { silent=true, buffer=true })
-
-          vim.keymap.set('n', 'q', function()
-            vim.fn['ddu#ui#do_action']('quit')
-          end, { silent=true, buffer=true })
-        end
+      vim.fn["ddu#custom#patch_local"]("buffers", {
+        sources = {
+          {
+            name = "buffer",
+          },
+        },
       })
 
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = 'ddu-ff-filter',
-        callback = function(ev)
-          vim.keymap.set('i', '<CR>',
-            "<ESC><Cmd>call ddu#ui#do_action(\"closeFilterWindow\")<CR>",
-            { silent=true, buffer=true })
-
-          vim.keymap.set('n', '<CR>', function()
-            vim.fn['ddu#ui#do_action']('closeFilterWindow')
-          end, { silent=true, buffer=true })
-
-          vim.keymap.set('n', 'q', function()
-            vim.fn['ddu#ui#do_action']('closeFilterWindow')
-          end, { silent=true, buffer=true })
-        end
+      vim.fn["ddu#custom#patch_local"]("rg", {
+        sources = {
+          {
+            name = "rg",
+            params = {
+              args = { "--json", "--hidden" },
+            },
+          },
+        },
       })
 
-      vim.keymap.set('n', ',f', function()
-        vim.fn['ddu#start']()
-      end)
-
-      vim.keymap.set('n', ',b', function()
-        vim.fn['ddu#start']({
-          sources = {
-            { name = 'buffer',
-              params = {}
-            }
-          }
-        })
-      end)
-
-      vim.keymap.set('v', ',g', function()
+      vim.keymap.set("n", ",f", function() vim.fn["ddu#start"]({ name = "files" }) end)
+      vim.keymap.set("n", ",b", function() vim.fn["ddu#start"]({ name = "buffers" }) end)
+      vim.keymap.set("v", ",g", function()
         local lines = get_current_buf_text()
         local text = table.concat(lines, "\n")
-
-        vim.fn['ddu#start']({
-          sources = {
-            {
-              name = 'rg',
-              params = {
-                input = text
-              },
-            },
-          }
-        })
+        vim.fn["ddu#start"]({ name = "rg", sourceParams = { rg = { input = text } } })
       end)
-
-      vim.keymap.set('n', ',g', function()
-        text = vim.fn.input("keyword: ")
-        vim.fn['ddu#start']({
-          sources = {
-            {
-              name = 'rg',
-              params = {
-                input = text
-              },
-            },
-          }
-        })
-      end)
-
-      vim.keymap.set('n', ',s', function()
-        vim.fn['ddu#start']({
-          sources = {
-            {
-              name = 'sample',
-              params = {},
-            },
-          },
-        })
-      end)
-
-    end,
-  },
-  {
-    "Shougo/ddu-ui-filer",
-    dependencies = {
-      "Shougo/ddu-source-file",
-      "Shougo/ddu-column-filename",
-      "Shougo/ddu.vim",
-    },
-    config = function()
-      vim.keymap.set('n', ',q', function()
-        vim.fn['ddu#start']({
-          ui = 'filer',
-          uiParams = {
-            filer = {
-              split = 'vertical',
-              splitDirection = 'topleft',
-              winWidth = 32,
-            }
-          },
-          sources = { { name = 'file', params = {}, } },
-          actionOptions = {
-            narrow = {
-              quit = false,
-            },
-          },
-          sourceOptions = {
-            _ = {
-              columns = { 'filename' },
-            },
-          },
-          kindOptions = {
-            file = {
-              defaultAction = 'open',
-            },
-          },
-        })
+      vim.keymap.set("n", ",g", function()
+        local text = vim.fn.input("keyword: ")
+        vim.fn["ddu#start"]({ name = "rg", sourceParams = { rg = { input = text } } })
       end)
 
 
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = 'ddu-filer',
+      vim.api.nvim_create_autocmd({ "FileType" }, {
+        pattern = { "ddu-ff" },
         callback = function(ev)
-          vim.keymap.set('n', '<CR>', function()
-            if vim.fn.line('.') == 1 then
-              vim.fn['ddu#ui#filer#do_action']('itemAction', { name = 'narrow', params = { path = '..' } })
-              return
-            end
-
-
-            local item = vim.fn['ddu#ui#get_item']()
-            if (item['isTree']) then
-              vim.fn['ddu#ui#filer#do_action']('itemAction', { name = 'narrow' } )
-            else
-              vim.fn['ddu#ui#filer#do_action']('itemAction', { name = 'open' })
-            end
-          end, { silent=true, buffer=true })
-
-          vim.keymap.set('n', '<Space>', function()
-            vim.fn['ddu#ui#filer#do_action']('toggleSelectItem')
-          end, { silent=true, buffer=true })
-
-          vim.keymap.set('n', 'o', function()
-            vim.fn['ddu#ui#filer#do_action']('expandItem', { mode = 'toggle' })
-          end, { silent=true, buffer=true })
-
-          vim.keymap.set('n', 'q', function()
-            vim.fn['ddu#ui#filer#do_action']('quit')
-          end, { silent=true, buffer=true })
+          vim.keymap.set("n", "<CR>", function() vim.fn["ddu#ui#do_action"]("itemAction") end, { buffer = true })
+          vim.keymap.set("n", "q", function() vim.fn["ddu#ui#do_action"]("quit") end, { buffer = true })
+          vim.keymap.set("n", "i", function() vim.fn["ddu#ui#do_action"]("openFilterWindow") end, { buffer = true })
         end
       })
-
+      vim.api.nvim_create_autocmd({ "FileType" }, {
+        pattern = { "ddu-ff-filter"},
+        callback = function(ev)
+          vim.keymap.set("i", "<CR>", function() 
+            vim.cmd('stopinsert')
+            vim.fn["ddu#ui#do_action"]("closeFilterWindow")
+          end, { buffer = true })
+          vim.keymap.set("n", "<CR>", function()
+            vim.fn["ddu#ui#do_action"]("closeFilterWindow")
+          end, { buffer = true })
+        end
+      })
     end,
   }
 }
